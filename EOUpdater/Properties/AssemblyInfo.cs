d7411da116d780778be08721e016dd32d643a566
silfumus <silfumus@users.noreply.github.com>
2017-11-26 11:33:48 +0700

    Update assembly info

--- EOUpdater/Properties/AssemblyInfo.cs
+++ EOUpdater/Properties/AssemblyInfo.cs
@@ -5,8 +5,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EOUpdater")]
-[assembly: AssemblyDescription("Updater for Electronic Observer")]
+[assembly: AssemblyTitle("Updater for Electronic Observer")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("silfumus")]
 [assembly: AssemblyProduct("EOUpdater")]
