26c3329e723dabf8529e49d1a543118374ccba06
silfumus <silfumus@users.noreply.github.com>
2017-12-06 23:29:13 +0700

    Clarify battle phase

--- ElectronicObserver/Data/Battle/Phase/PhaseBase.cs
+++ ElectronicObserver/Data/Battle/Phase/PhaseBase.cs
@@ -25,9 +25,9 @@ namespace ElectronicObserver.Data.Battle.Phase
 				case "噴式基地航空隊攻撃": return "Land-based Jet Air Attack";
 				case "噴式航空戦": return "Jet Air Attack";
 				case "基地航空隊攻撃": return "Land-based Air Attack";
-				case "防空戦": return "Air Raid";
+				case "防空戦": return "Land Base Air Raid";
 				case "航空戦": return "Air Battle";
-				case "空襲戦": return "Air Raid Battle";
+				case "空襲戦": return "Air Raid";
 				case "第一次航空戦": return "Air Attack, 1st"; 
 				case "第二次航空戦": return "Air Attack, 2nd";
 				case "支援攻撃": return "Support Expedition";
