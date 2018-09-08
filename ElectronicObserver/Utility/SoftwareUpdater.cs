938e2c7610c87debd7ba1389e04d9d8a11801234
silfumus <silfumus@users.noreply.github.com>
2018-09-08 14:24:33 +0700

    Refactor translation data handling

--- ElectronicObserver/Utility/SoftwareUpdater.cs
+++ ElectronicObserver/Utility/SoftwareUpdater.cs
@@ -11,28 +11,17 @@ namespace ElectronicObserver.Utility
 {
     internal class SoftwareUpdater
 	{
-		public static bool UpdateRestart = false;
-		
 		internal static readonly string AppDataFolder =
 			Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\\ElectronicObserver";
 		internal static readonly string TranslationFolder = AppDataFolder + "\\Translations";
 		private static readonly Uri UpdateUrl =
 			new Uri("https://raw.githubusercontent.com/silfumus/ryuukitsune.github.io/master/Translations/en-US/update.json");
-
-		internal static string EqVer { get; set; } = "0.0.0";
-		internal static string EqTypeVer { get; set; } = "0.0.0";
-		internal static string ExpVer { get; set; } = "0.0.0";
-		internal static string OpVer { get; set; } = "0.0.0";
-		internal static string QuestVer { get; set; } = "0.0.0";
-		internal static string ShipVer { get; set; } = "0.0.0";
-		internal static string ShipTypeVer { get; set; } = "0.0.0";
 		internal static string MaintDate { get; set; } = string.Empty;
 		internal static int MaintState { get; set; }
 
-		internal static string ZipUrl = string.Empty;
-		internal static string DownloadHash = string.Empty;
-		private static bool _isChecked;
-
+		//internal static string ZipUrl = string.Empty;
+		//internal static string DownloadHash = string.Empty;
+		private static bool _isChecked = false;
 
 		public static void UpdateSoftware()
 		{
@@ -45,22 +34,19 @@ namespace ElectronicObserver.Utility
 			var updaterFile = AppDomain.CurrentDomain.SetupInformation.ApplicationBase + @"\EOUpdater.exe";
 			if (!File.Exists(updaterFile))
 			{
+				var updater = new Process
+				{
+					StartInfo =
+					{
+						FileName = updaterFile,
+						UseShellExecute = false,
+						CreateNoWindow = false
+					}
+				};
+				updater.StartInfo.Arguments = "--restart";
+				updater.Start();
 				Logger.Add(2, "Updater started. Close EO after it has finished downloading the update.");
 			}
-			var updater = new Process
-			{
-				StartInfo =
-				{
-					FileName = updaterFile,
-					UseShellExecute = false,
-					CreateNoWindow = false
-		}
-			};
-			if (!UpdateRestart)
-				updater.StartInfo.Arguments = "--restart";
-
-			updater.Start();
-			Logger.Add(2, "Updater started. Close EO after it has finished downloading the update.");
 		}
 
 		private static void DownloadUpdater()
@@ -88,29 +74,27 @@ namespace ElectronicObserver.Utility
 		    if (_isChecked) return;
 			try
 			{
-			    int nodeVer;
-			    using (var client = WebRequest.Create(UpdateUrl).GetResponse())
+
+				using (var client = WebRequest.Create(UpdateUrl).GetResponse())
 				{
 					var updateData = client.GetResponseStream();
 					var json = DynamicJson.Parse(updateData);
 
-					ZipUrl = json.url;
-					DownloadHash = json.hash;
+					//ZipUrl = json.url;
+					//DownloadHash = json.hash;
 					MaintDate = json.kancolle_mt;
 					MaintState = (int)json.event_state;
 
-					EqVer = json.tl_ver.equipment;
-					EqTypeVer = json.tl_ver.equipment_type;
-					ExpVer = json.tl_ver.expedition;
-					OpVer = json.tl_ver.operation;
-					QuestVer = json.tl_ver.quest;
-					ShipVer = json.tl_ver.ship;
-					ShipTypeVer = json.tl_ver.ship_type;
-				    nodeVer = (int)json.tl_ver.nodes;
+					CheckDataVersion(TranslationFile.Equipment, json.tl_ver.equipment);
+					CheckDataVersion(TranslationFile.EquipmentTypes, json.tl_ver.equipment_type);
+					CheckDataVersion(TranslationFile.Expeditions, json.tl_ver.expedition);
+					CheckDataVersion(TranslationFile.Operations, json.tl_ver.operation);
+					CheckDataVersion(TranslationFile.Quests, json.tl_ver.quest);
+					CheckDataVersion(TranslationFile.Ships, json.tl_ver.ship);
+					CheckDataVersion(TranslationFile.ShipTypes, json.tl_ver.ship_type);
+					CheckDataVersion("nodes.json", (int)json.tl_ver.nodes);
 				}
-
-			    if (nodeVer != CheckDataVersion("nodes.json"))
-			        DownloadData("nodes.json");
+				
 			}
 			catch (Exception e)
 			{
@@ -120,41 +104,46 @@ namespace ElectronicObserver.Utility
 			_isChecked = true;
 	    }
 
-	    public static int CheckDataVersion(string filename)
+	    public static void CheckDataVersion(string filename, int latestVer)
 	    {
 	        var source = TranslationFolder + $"\\{filename}";
-	        if (!File.Exists(source))
+		    var currentVer = 0;
+			if (!File.Exists(source))
 	            DownloadData(filename);
-	        try
-	        {
-	            using (var sr = new StreamReader(source))
-	            {
-	                var json = DynamicJson.Parse(sr.ReadToEnd());
-	                return (int)json.Revision;
-	            }
-	        }
-	        catch (Exception)
-	        {
-	            return 0;
-	        }
-        }
+		    try
+		    {
+			    using (var sr = new StreamReader(source))
+			    {
+				    var json = DynamicJson.Parse(sr.ReadToEnd());
+				    currentVer = (int) json.Revision;
+			    }
+		    }
+		    catch (Exception e)
+		    {
+			    Logger.Add(3, "Error while checking translation data. " + e);
+		    }
+		    if (latestVer != currentVer)
+			    DownloadData(filename);
+		}
 
-	    public static string CheckDataVersion(TranslationFile filename)
+	    public static void CheckDataVersion(TranslationFile filename, string latestVer)
 	    {
 	        var source = TranslationFolder + $"\\{filename}.xml";
-            if (!File.Exists(source))
+		    var currentVer = "0.0.0";
+			if (!File.Exists(source))
 	            DownloadData(filename);
-            Console.WriteLine(source);
-	        try
-	        {
-	            var xml = XDocument.Load(source);
-	            return xml.Root.Attribute("Version").Value;
-	        }
-	        catch (Exception)
-	        {
-	            return "0.0.0";
-	        }
-        }
+		    try
+		    {
+			    var xml = XDocument.Load(source);
+			    currentVer = xml.Root.Attribute("Version").Value;
+		    }
+		    catch (Exception e)
+			{
+				Logger.Add(3, "Error while checking translation data. " + e);
+			}
+		    if (latestVer != currentVer)
+			    DownloadData(filename);
+		}
 
         private static string GetHash(string filename)
 		{
