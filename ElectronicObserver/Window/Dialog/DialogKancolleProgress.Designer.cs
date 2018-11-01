2b0217acf9772aabe63a24f6b85f86d7229b05df
MyAngelKamikaze <40002167+myangelkamikaze@users.noreply.github.com>
2018-11-01 17:58:18 +0900

    HeilH Nice

--- ElectronicObserver/Window/Dialog/DialogKancolleProgress.Designer.cs
+++ ElectronicObserver/Window/Dialog/DialogKancolleProgress.Designer.cs
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ShipList = new System.Windows.Forms.DataGridView();
             this.filler0 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.DD = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.filler1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -52,20 +52,20 @@
             this.XX = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.filler10 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Summary = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ShipList)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // ShipList
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.AllowUserToResizeColumns = false;
-            this.dataGridView1.AllowUserToResizeRows = false;
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
-            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
-            this.dataGridView1.CausesValidation = false;
-            this.dataGridView1.ColumnHeadersVisible = false;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ShipList.AllowUserToAddRows = false;
+            this.ShipList.AllowUserToDeleteRows = false;
+            this.ShipList.AllowUserToResizeColumns = false;
+            this.ShipList.AllowUserToResizeRows = false;
+            this.ShipList.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.ShipList.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.ShipList.CausesValidation = false;
+            this.ShipList.ColumnHeadersVisible = false;
+            this.ShipList.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.filler0,
             this.DD,
             this.filler1,
@@ -95,20 +95,20 @@
             dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
             dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
             dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
-            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
-            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.RowHeadersVisible = false;
-            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
-            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
-            this.dataGridView1.ShowCellErrors = false;
-            this.dataGridView1.ShowCellToolTips = false;
-            this.dataGridView1.ShowEditingIcon = false;
-            this.dataGridView1.ShowRowErrors = false;
-            this.dataGridView1.Size = new System.Drawing.Size(1377, 714);
-            this.dataGridView1.TabIndex = 0;
+            this.ShipList.DefaultCellStyle = dataGridViewCellStyle1;
+            this.ShipList.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ShipList.Location = new System.Drawing.Point(0, 0);
+            this.ShipList.Name = "ShipList";
+            this.ShipList.ReadOnly = true;
+            this.ShipList.RowHeadersVisible = false;
+            this.ShipList.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.ShipList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.CellSelect;
+            this.ShipList.ShowCellErrors = false;
+            this.ShipList.ShowCellToolTips = false;
+            this.ShipList.ShowEditingIcon = false;
+            this.ShipList.ShowRowErrors = false;
+            this.ShipList.Size = new System.Drawing.Size(1377, 714);
+            this.ShipList.TabIndex = 0;
             // 
             // filler0
             // 
@@ -288,17 +288,17 @@
             // 
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(1377, 714);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.ShipList);
             this.DoubleBuffered = true;
             this.Name = "DialogKancolleProgress";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ShipList)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView ShipList;
         private System.Windows.Forms.DataGridViewTextBoxColumn filler0;
         private System.Windows.Forms.DataGridViewTextBoxColumn DD;
         private System.Windows.Forms.DataGridViewTextBoxColumn filler1;
