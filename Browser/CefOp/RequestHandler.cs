fd01c1f3fcbe2353c6258a4d3d4bf2e05fa0b6c9
gre4bee <gre4bee@gmail.com>
2019-09-01 15:42:24 +0800

    Remove range request header for BGM Caching
    --fix for BGM not being cached

--- Browser/CefOp/RequestHandler.cs
+++ Browser/CefOp/RequestHandler.cs
@@ -44,6 +44,10 @@ namespace Browser.CefOp
 			{
 				return CefReturnValue.Cancel;
 			}
+			// remove range request header to allow bgm caching
+			var headers = request.Headers;
+			headers.Remove("Range");
+			request.Headers = headers;
 
 			return base.OnBeforeResourceLoad(browserControl, browser, frame, request, callback);
 		}
