29465442e73db118878be9328036e4ad4b27f6d6
myangelkamikaze <myangelkamikaze@gmail.com>
2019-10-01 12:24:56 +0900

    tl fix

--- ElectronicObserver/Observer/kcsapi/api_req_kousyou/createitem.cs
+++ ElectronicObserver/Observer/kcsapi/api_req_kousyou/createitem.cs
@@ -31,12 +31,12 @@ namespace ElectronicObserver.Observer.kcsapi.api_req_kousyou
 			if (Utility.Configuration.Config.Log.ShowSpoiler)
 			{
 				//Utility.Logger.Add(2, $"開発結果: {string.Join(", ", dev.Results)} ({dev.Fuel}/{dev.Ammo}/{dev.Steel}/{dev.Bauxite} 秘書艦: {db.Fleet[1].MembersInstance[0].NameWithLevel})");
-                // todo translate
+
 				foreach (var result in dev.Results)
 				{
 					if (result.IsSucceeded)
 					{
-						Utility.Logger.Add(2, string.Format("{0}「{1}」の開発に成功しました。({2}/{3}/{4}/{5} 秘書艦: {6})",
+						Utility.Logger.Add(2, string.Format(LoggerRes.CreatedItem,
 							result.MasterEquipment.CategoryTypeInstance.Name,
 							result.MasterEquipment.Name,
 							dev.Fuel, dev.Ammo, dev.Steel, dev.Bauxite,
@@ -44,7 +44,7 @@ namespace ElectronicObserver.Observer.kcsapi.api_req_kousyou
 					}
 					else
 					{
-						Utility.Logger.Add(2, string.Format("開発に失敗しました。({0}/{1}/{2}/{3} 秘書艦: {4})",
+                        Utility.Logger.Add(2, string.Format(LoggerRes.ItemCreationFailed,
 							dev.Fuel, dev.Ammo, dev.Steel, dev.Bauxite,
 							db.Fleet[1].MembersInstance[0].NameWithLevel));
 					}
