bd91db14d44d9fe92b055131962f6062c874a5e1
myangelkamikaze <myangelkamikaze@gmail.com>
2020-05-01 10:15:10 +0900

    version update

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -38,14 +38,14 @@ namespace ElectronicObserver.Utility
 		/// <summary>
 		/// バージョン(英語)
 		/// </summary>
-		public static string VersionEnglish => "4.4.1.7";
+		public static string VersionEnglish => "4.5.1";
 
 
 
 		/// <summary>
 		/// 更新日時
 		/// </summary>
-		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2020/04/26 20:00:00");
+		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2020/05/01 10:00:00");
 
 
 
