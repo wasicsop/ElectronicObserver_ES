830e885208d557ceb3060aad82e33254537649b5
Jebzou <22751386+Jebzou@users.noreply.github.com>
2020-07-02 18:59:09 +0200

    Merge

---
+++ ElectronicObserver/Window/Dialog/DialogTsunDb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ElectronicObserver.Window.Dialog
+{
+	public partial class DialogTsunDb : Form
+	{
+		public DialogTsunDb()
+		{
+			InitializeComponent();
+		}
+	}
+}
