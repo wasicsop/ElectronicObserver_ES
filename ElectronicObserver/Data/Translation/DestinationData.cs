80139f298e799f6f3f1c804c35a7fab531089210
Reza Budiraharjo <silfumus@users.noreply.github.com>
2020-07-17 20:35:20 +0700

    Use properties instead of fields

--- ElectronicObserver/Data/Translation/DestinationData.cs
+++ ElectronicObserver/Data/Translation/DestinationData.cs
@@ -12,18 +12,32 @@ namespace ElectronicObserver.Data.Translation
 	{
 		private string DefaultFilePath = TranslationManager.WorkingFolder + @"\destination.json";
 
-		private Dictionary<string, string> DestinationList;
+		private IDDictionary<Destinations> DestinationList;
 
 		/// <summary>
 		/// Destination Node Letter ID (e.g: 1-1-A)
 		/// </summary>
-		public string DisplayID(int map, int area, int destination)
+		public string DisplayID(int mapAreaID, int mapInfoID, int destination)
 		{
-			var dest = map.ToString() + "-" + area.ToString() + "-" + destination.ToString();
-			return IsConverted(dest) ? DestinationList[dest] : destination.ToString();
+			int hash = GetHashCode(mapAreaID, mapInfoID, destination);
+			return IsLoaded && DestinationList.TryGetValue(hash, out Destinations value) ? value.DestinationDisplayID : destination.ToString();
 		}
-		public bool IsConverted(string dest)
-			=> Configuration.Config.UI.UseOriginalNodeId == false && DestinationList != null && DestinationList.ContainsKey(dest);
+
+		public string PreviousID(int mapAreaID, int mapInfoID, int destination)
+		{
+			int hash = GetHashCode(mapAreaID, mapInfoID, destination);
+			return IsLoaded && DestinationList.TryGetValue(hash, out Destinations value) ? value.PreviousDisplayID : "";
+		}
+
+		private bool IsLoaded => Configuration.Config.UI.UseOriginalNodeId == false && DestinationList != null;
+
+		private int GetHashCode(int mapAreaID, int mapInfoID, int destination)
+		{
+			int hash = mapAreaID;
+			hash = hash * 397 ^ mapInfoID;
+			hash = hash * 397 ^ destination;
+			return hash;
+		}
 
 		public override void Initialize()
 		{
@@ -35,9 +49,9 @@ namespace ElectronicObserver.Data.Translation
 			Initialize();
 		}
 
-		public Dictionary<string, string> LoadDictionary(string path)
+		private IDDictionary<Destinations> LoadDictionary(string path)
 		{
-			var dict = new Dictionary<string, string>();
+			var dict = new IDDictionary<Destinations>();
 
 			var json = Load(path);
 			if (json == null) return dict;
@@ -48,13 +62,39 @@ namespace ElectronicObserver.Data.Translation
 				var destinations = JsonObject.Parse(map.Value.ToString());
 				foreach (KeyValuePair<string, dynamic> dest in destinations)
 				{
-					string area = map.Key.Remove(0, 6);
-					string destination = $"{area}-{dest.Key}";
-					string displayID = dest.Value[1];
-					dict.TryAdd(destination, displayID);
+					string[] world = map.Key.Remove(0, 6).Split('-');
+					int mapAreaID = int.Parse(world[0]);
+					int mapInfoID = int.Parse(world[1]);
+					int destination = int.Parse(dest.Key);
+					string previousDisplayID = dest.Value[0];
+					string destinationDisplayID = dest.Value[1];
+
+					int hash = GetHashCode(mapAreaID, mapInfoID, destination);
+
+					var item = new Destinations
+					{
+						ID = hash,
+						MapAreaID = mapAreaID,
+						MapInfoID = mapInfoID,
+						Destination = destination,
+						PreviousDisplayID = previousDisplayID,
+						DestinationDisplayID = destinationDisplayID
+					};
+
+					dict.Add(item);
 				}
 			}
 			return dict;
+		}
+
+		private class Destinations : IIdentifiable
+		{
+			public int ID { get; set; }
+			public int MapAreaID { get; set; }
+			public int MapInfoID { get; set; }
+			public int Destination { get; set; }
+			public string PreviousDisplayID { get; set; }
+			public string DestinationDisplayID { get; set; }
 		}
 	}
 }
