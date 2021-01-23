fc5f432a3ae53854f800c8deda669d34787dd9db
myangelkamikaze <myangelkamikaze@gmail.com>
2021-01-23 16:00:17 +0900

    version update

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -36,13 +36,13 @@ namespace ElectronicObserver.Utility
 		/// <summary>
 		/// バージョン(英語)
 		/// </summary>
-		public static string VersionEnglish => "4.6.4.1";
+		public static string VersionEnglish => "4.6.5";
 
 
 		/// <summary>
 		/// 更新日時
 		/// </summary>
-		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2021/01/16 21:00:00");
+		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2021/01/23 16:00:00");
 
 
 		private static System.Net.WebClient? Client { get; set; }
