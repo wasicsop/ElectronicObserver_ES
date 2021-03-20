6685188f480dfdeaee6b7c41dab4c10a8a836d3b
myangelkamikaze <myangelkamikaze@gmail.com>
2021-03-20 16:05:22 +0900

    wpf app

---
+++ ElectronicObserver/FormMainWpf.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ElectronicObserver
+{
+	/// <summary>
+	/// Interaction logic for FormMainWpf.xaml
+	/// </summary>
+	public partial class FormMainWpf : System.Windows.Window
+	{
+		public FormMainWpf()
+		{
+			InitializeComponent();
+		}
+	}
+}
