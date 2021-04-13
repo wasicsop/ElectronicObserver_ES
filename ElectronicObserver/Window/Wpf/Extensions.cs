dec44690ad12132374eae89f182f64313dc9a6ec
myangelkamikaze <myangelkamikaze@gmail.com>
2021-04-14 00:41:53 +0900

    add visibility extension and fix color to brush extension

--- ElectronicObserver/Window/Wpf/Extensions.cs
+++ ElectronicObserver/Window/Wpf/Extensions.cs
@@ -1,10 +1,17 @@
-﻿using System.Windows.Media;
+﻿using System.Windows;
+using System.Windows.Media;
 
 namespace ElectronicObserver.Window.Wpf
 {
 	public static class Extensions
 	{
+		public static Visibility ToVisibility(this bool visible) => visible switch
+		{
+			true => Visibility.Visible,
+			_ => Visibility.Collapsed
+		};
+
 		public static SolidColorBrush ToBrush(this System.Drawing.Color color) =>
-			new(Color.FromRgb(color.R, color.G, color.B));
+			new(Color.FromArgb(color.A, color.R, color.G, color.B));
 	}
 }
