503dda7221666ecdf75d3312119da127ebed431d
gre4bee <gre4bee@gmail.com>
2021-04-22 22:18:45 +0800

    Adjust URL to kcwiki bypass
    --change url to kcwiki bypass

--- Browser/CefOp/GadgetUrlHandler.cs
+++ Browser/CefOp/GadgetUrlHandler.cs
@@ -8,7 +8,7 @@ namespace Browser.CefOp
 		protected override IResponseFilter GetResourceResponseFilter(IWebBrowser chromiumWebBrowser, IBrowser browser, IFrame frame, IRequest request, IResponse response)
 		{
 			if (request.Url.Contains(@"gadget_html5"))
-				return new GadgetReplaceFilter("203.104.209.7/gadget_html5/", "18.176.189.52/gadget_html5/");
+				return new GadgetReplaceFilter("http://203.104.209.7/gadget_html5/", "https://kcwiki.github.io/cache/gadget_html5/");
 			return base.GetResourceResponseFilter(chromiumWebBrowser, browser, frame, request, response);
 		}
 	}
