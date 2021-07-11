731f85d3e5c6b0535467c436ea16ba5f2cff3ff4
myangelkamikaze <myangelkamikaze@gmail.com>
2021-07-11 11:55:53 +0900

    4.6.8.4

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -36,13 +36,13 @@ namespace ElectronicObserver.Utility
 		/// <summary>
 		/// バージョン(英語)
 		/// </summary>
-		public static string VersionEnglish => "4.6.8.3";
+		public static string VersionEnglish => "4.6.8.4";
 
 
 		/// <summary>
 		/// 更新日時
 		/// </summary>
-		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2021/07/10 12:00:00");
+		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2021/07/11 12:00:00");
 
 
 		private static System.Net.WebClient? Client { get; set; }
