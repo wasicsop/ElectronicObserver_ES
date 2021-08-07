b19fa0e4b4a2d88945cf8373cd923196f993034c
myangelkamikaze <myangelkamikaze@gmail.com>
2021-08-07 16:05:30 +0900

    QuestView localization (partial) #67 wpf version doesn't exist yet

---
+++ ElectronicObserver/ViewModels/Translations/FormQuestTranslationViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace ElectronicObserver.ViewModels.Translations
+{
+	public class FormQuestPropertiesTranslationViewModel : TranslationBaseViewModel
+	{
+		public string MenuProgress_Reset => Properties.Window.FormQuest.MenuProgress_Reset.Replace("_", "__").Replace("&", "_");
+
+		public string MenuMain_QuestFilter => Properties.Window.FormQuest.MenuMain_QuestFilter.Replace("_", "__").Replace("&", "_");
+
+		public string MenuMain_GoogleQuest => Properties.Window.FormQuest.LookUpQuestOnWeb.Replace("_", "__").Replace("&", "_");
+
+		public string MenuMain_KcwikiQuest => Properties.Window.FormQuest.MenuMain_KcwikiQuest.Replace("_", "__").Replace("&", "_");
+
+		public string ManuMain_QuestTitle => Properties.Window.FormQuest.ManuMain_QuestTitle.Replace("_", "__").Replace("&", "_");
+
+		public string ManuMain_QuestDescription => Properties.Window.FormQuest.ManuMain_QuestDescription.Replace("_", "__").Replace("&", "_");
+
+		public string ManuMain_QuestTranslate => Properties.Window.FormQuest.ManuMain_QuestTranslate.Replace("_", "__").Replace("&", "_");
+	}
+}
---
+++ ElectronicObserver/ViewModels/Translations/GeneralResTranslationViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ElectronicObserver.Window;
+
+namespace ElectronicObserver.ViewModels.Translations
+{
+	public class GeneralResTranslationViewModel : TranslationBaseViewModel
+	{
+		public string QuestView_Type => GeneralRes.Type.Replace("_", "__").Replace("&", "_");
+		public string QuestView_Category => GeneralRes.Category.Replace("_", "__").Replace("&", "_");
+		public string QuestView_Name => GeneralRes.QuestName.Replace("_", "__").Replace("&", "_");
+		public string QuestView_Progress => GeneralRes.Progress.Replace("_", "__").Replace("&", "_");
+
+		public string MenuProgress_Increment => GeneralRes.IncrementByOne.Replace("_", "__").Replace("&", "_");
+		public string MenuProgress_Decrement => GeneralRes.DecrementByOne.Replace("_", "__").Replace("&", "_");
+
+		public string MenuMain_ColumnFilter => GeneralRes.FilterBy.Replace("_", "__").Replace("&", "_");
+		public string MenuMain_ColumnFilter_State => GeneralRes.InProgressFilter.Replace("_", "__").Replace("&", "_");
+		public string MenuMain_ColumnFilter_Type => GeneralRes.TypeFilter.Replace("_", "__").Replace("&", "_");
+		public string MenuMain_ColumnFilter_Category => GeneralRes.CategoryFilter.Replace("_", "__").Replace("&", "_");
+		public string MenuMain_ColumnFilter_Name => GeneralRes.NameFilter.Replace("_", "__").Replace("&", "_");
+		public string MenuMain_ColumnFilter_Progress => GeneralRes.ProgressFilter.Replace("_", "__").Replace("&", "_");
+		public string MenuMain_Initialize => GeneralRes.Initialize.Replace("_", "__").Replace("&", "_");
+
+		public string MenuMain_ShowWeekly => GeneralRes.ShowWeekly.Replace("_", "__").Replace("&", "_");
+		public string MenuMain_ShowDaily => GeneralRes.ShowDaily.Replace("_", "__").Replace("&", "_");
+		public string MenuMain_ShowOnce => GeneralRes.ShowOneTime.Replace("_", "__").Replace("&", "_");
+		public string MenuMain_ShowRunningOnly => GeneralRes.ShowInProgressOnly.Replace("_", "__").Replace("&", "_");
+		public string MenuMain_ShowMonthly => GeneralRes.ShowMonthly.Replace("_", "__").Replace("&", "_");
+		public string MenuMain_ShowOther => GeneralRes.ShowOther.Replace("_", "__").Replace("&", "_");
+
+		public string Quest => GeneralRes.Quest.Replace("_", "__").Replace("&", "_");
+
+	}
+}
