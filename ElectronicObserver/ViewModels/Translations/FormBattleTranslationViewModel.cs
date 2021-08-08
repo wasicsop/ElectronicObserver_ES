6e09941dc13d6a21013a3761e5338295a570d1af
myangelkamikaze <myangelkamikaze@gmail.com>
2021-08-08 23:55:45 +0900

    bugfix BattleView localization #67

--- ElectronicObserver/ViewModels/Translations/FormBattleTranslationViewModel.cs
+++ ElectronicObserver/ViewModels/Translations/FormBattleTranslationViewModel.cs
@@ -4,48 +4,48 @@ namespace ElectronicObserver.ViewModels.Translations
 {
 	public class FormBattleTranslationViewModel : TranslationBaseViewModel
 	{
-		public string ABText => Properties.Window.FormBattle.ABText;
-		public string AerialPhaseJet => Properties.Window.FormBattle.AerialPhaseJet;
-		public string AerialPhase1 => Properties.Window.FormBattle.AerialPhase1;
-		public string AerialPhase2 => Properties.Window.FormBattle.AerialPhase2;
-		public string AirDefense => Properties.Window.FormBattle.AirDefense;
-		public string Contact => Properties.Window.FormBattle.Contact;
-		public string None => Properties.Window.FormBattle.None;
-		public string AACI => Properties.Window.FormBattle.AACI;
-		public string AACIType => Properties.Window.FormBattle.AACIType;
-		public string DidNotActivate => Properties.Window.FormBattle.DidNotActivate;
-		public string AirBase => Properties.Window.FormBattle.AirBase;
-		public string SupportExpedition => Properties.Window.FormBattle.SupportExpedition;
-		public string FleetFriendShort => Properties.Window.FormBattle.FleetFriendShort;
+		public string ABText => Properties.Window.FormBattle.ABText.Replace("_", "__").Replace("&", "_");
+		public string AerialPhaseJet => Properties.Window.FormBattle.AerialPhaseJet.Replace("_", "__").Replace("&", "_");
+		public string AerialPhase1 => Properties.Window.FormBattle.AerialPhase1.Replace("_", "__").Replace("&", "_");
+		public string AerialPhase2 => Properties.Window.FormBattle.AerialPhase2.Replace("_", "__").Replace("&", "_");
+		public string AirDefense => Properties.Window.FormBattle.AirDefense.Replace("_", "__").Replace("&", "_");
+		public string Contact => Properties.Window.FormBattle.Contact.Replace("_", "__").Replace("&", "_");
+		public string None => Properties.Window.FormBattle.None.Replace("_", "__").Replace("&", "_");
+		public string AACI => Properties.Window.FormBattle.AACI.Replace("_", "__").Replace("&", "_");
+		public string AACIType => Properties.Window.FormBattle.AACIType.Replace("_", "__").Replace("&", "_");
+		public string DidNotActivate => Properties.Window.FormBattle.DidNotActivate.Replace("_", "__").Replace("&", "_");
+		public string AirBase => Properties.Window.FormBattle.AirBase.Replace("_", "__").Replace("&", "_");
+		public string SupportExpedition => Properties.Window.FormBattle.SupportExpedition.Replace("_", "__").Replace("&", "_");
+		public string FleetFriendShort => Properties.Window.FormBattle.FleetFriendShort.Replace("_", "__").Replace("&", "_");
 
-		public string RightClickMenu_ShowBattleDetail => GeneralRes.RightClickMenu_ShowBattleDetail;
-		public string RightClickMenu_ShowBattleResult => GeneralRes.RightClickMenu_ShowBattleResult;
+		public string RightClickMenu_ShowBattleDetail => GeneralRes.RightClickMenu_ShowBattleDetail.Replace("_", "__").Replace("&", "_");
+		public string RightClickMenu_ShowBattleResult => GeneralRes.RightClickMenu_ShowBattleResult.Replace("_", "__").Replace("&", "_");
 
-		public string FormationFriend => GeneralRes.FriendlyFormation;
-		public string Formation => GeneralRes.EncounterType;
-		public string FormationEnemy => GeneralRes.EnemyFormation;
+		public string FormationFriend => GeneralRes.FriendlyFormation.Replace("_", "__").Replace("&", "_");
+		public string Formation => GeneralRes.EncounterType.Replace("_", "__").Replace("&", "_");
+		public string FormationEnemy => GeneralRes.EnemyFormation.Replace("_", "__").Replace("&", "_");
 
-		public string AirStage2Friend => GeneralRes.ShotDown;
-		public string AACutin => GeneralRes.AntiAir;
-		public string AirStage2Enemy => GeneralRes.ShotDown;
+		public string AirStage2Friend => GeneralRes.ShotDown.Replace("_", "__").Replace("&", "_");
+		public string AACutin => GeneralRes.AntiAir.Replace("_", "__").Replace("&", "_");
+		public string AirStage2Enemy => GeneralRes.ShotDown.Replace("_", "__").Replace("&", "_");
 
-		public string AirStage1Friend => GeneralRes.ShotDown;
-		public string AirSuperiority => GeneralRes.AirSuperiority;
-		public string AirStage1Enemy => GeneralRes.ShotDown;
+		public string AirStage1Friend => GeneralRes.ShotDown.Replace("_", "__").Replace("&", "_");
+		public string AirSuperiority => GeneralRes.AirSuperiority.Replace("_", "__").Replace("&", "_");
+		public string AirStage1Enemy => GeneralRes.ShotDown.Replace("_", "__").Replace("&", "_");
 
-		public string SearchingFriend => GeneralRes.FriendlyScout;
-		public string Searching => GeneralRes.Scouting;
-		public string SearchingEnemy => GeneralRes.EnemyScout;
+		public string SearchingFriend => GeneralRes.FriendlyScout.Replace("_", "__").Replace("&", "_");
+		public string Searching => GeneralRes.Scouting.Replace("_", "__").Replace("&", "_");
+		public string SearchingEnemy => GeneralRes.EnemyScout.Replace("_", "__").Replace("&", "_");
 
-		public string FleetFriend => Properties.Window.FormBattle.FleetFriend;
-		public string FleetFriendEscort => Properties.Window.FormBattle.FleetFriendEscort;
-		public string FleetEnemyEscort => Properties.Window.FormBattle.FleetEnemyEscort;
-		public string FleetEnemy => Properties.Window.FormBattle.FleetEnemy;
+		public string FleetFriend => Properties.Window.FormBattle.FleetFriend.Replace("_", "__").Replace("&", "_");
+		public string FleetFriendEscort => Properties.Window.FormBattle.FleetFriendEscort.Replace("_", "__").Replace("&", "_");
+		public string FleetEnemyEscort => Properties.Window.FormBattle.FleetEnemyEscort.Replace("_", "__").Replace("&", "_");
+		public string FleetEnemy => Properties.Window.FormBattle.FleetEnemy.Replace("_", "__").Replace("&", "_");
 
-		public string DamageFriend => Properties.Window.FormBattle.DamageFriend;
-		public string WinRank => Properties.Window.FormBattle.WinRank;
-		public string DamageEnemy => Properties.Window.FormBattle.DamageEnemy;
+		public string DamageFriend => Properties.Window.FormBattle.DamageFriend.Replace("_", "__").Replace("&", "_");
+		public string WinRank => Properties.Window.FormBattle.WinRank.Replace("_", "__").Replace("&", "_");
+		public string DamageEnemy => Properties.Window.FormBattle.DamageEnemy.Replace("_", "__").Replace("&", "_");
 
-		public string Title => GeneralRes.Battle;
+		public string Title => GeneralRes.Battle.Replace("_", "__").Replace("&", "_");
 	}
 }
