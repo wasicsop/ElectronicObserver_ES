1acf60690bc9485955540b4e92c2a00daddc6f81
myangelkamikaze <myangelkamikaze@gmail.com>
2021-09-25 18:31:49 +0900

    extract view template selector

---
+++ ElectronicObserver/Window/Wpf/ViewTemplateSelector.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ElectronicObserver.Window.Wpf.Arsenal;
+using ElectronicObserver.Window.Wpf.BaseAirCorps;
+using ElectronicObserver.Window.Wpf.Battle;
+using ElectronicObserver.Window.Wpf.Compass;
+using ElectronicObserver.Window.Wpf.Dock;
+using ElectronicObserver.Window.Wpf.Fleet;
+using ElectronicObserver.Window.Wpf.FleetOverview;
+using ElectronicObserver.Window.Wpf.FleetPreset;
+using ElectronicObserver.Window.Wpf.Headquarters;
+using ElectronicObserver.Window.Wpf.WinformsHost;
+
+namespace ElectronicObserver.Window.Wpf
+{
+	public class ViewTemplateSelector : DataTemplateSelector
+	{
+		public DataTemplate? Fleet { get; set; }
+		public DataTemplate? FleetOverview { get; set; }
+		public DataTemplate? Group { get; set; }
+		public DataTemplate? FleetPreset { get; set; }
+
+		public DataTemplate? Dock { get; set; }
+		public DataTemplate? Arsenal { get; set; }
+		public DataTemplate? BaseAirCorps { get; set; }
+
+		public DataTemplate? Headquarters { get; set; }
+		public DataTemplate? Quest { get; set; }
+		public DataTemplate? Information { get; set; }
+
+		public DataTemplate? Compass { get; set; }
+		public DataTemplate? Battle { get; set; }
+
+		public DataTemplate? BrowserHost { get; set; }
+		public DataTemplate? Log { get; set; }
+		public DataTemplate? Json { get; set; }
+
+		public DataTemplate? WinformsHost { get; set; }
+
+		public override DataTemplate? SelectTemplate(object item, DependencyObject container) => item switch
+		{
+			FleetViewModel => Fleet,
+			FleetOverviewViewModel => FleetOverview,
+			FleetPresetViewModel => FleetPreset,
+
+			DockViewModel => Dock,
+			ArsenalViewModel => Arsenal,
+			BaseAirCorpsViewModel => BaseAirCorps,
+
+			HeadquartersViewModel => Headquarters,
+
+			CompassViewModel => Compass,
+			BattleViewModel => Battle,
+
+			WinformsHostViewModel => WinformsHost,
+
+			_ => base.SelectTemplate(item, container)
+		};
+	}
+}
