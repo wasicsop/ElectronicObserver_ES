b01bbfd5cdce48c15dfc97178a0b2796108fd9f9
myangelkamikaze <myangelkamikaze@gmail.com>
2021-12-15 21:05:37 +0900

    add BooleanToHiddenConverter

---
+++ ElectronicObserver/Converters/BooleanToHiddenConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace ElectronicObserver.Converters;
+
+public class BooleanToHiddenConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		=> value switch
+		{
+			true => Visibility.Visible,
+			_ => Visibility.Hidden
+		};
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotSupportedException();
+	}
+}
