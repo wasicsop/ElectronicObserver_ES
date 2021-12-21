6977afe7ddf295f340a620426ebbda49de73820c
gre4bee <gre4bee@gmail.com>
2021-12-21 20:09:23 +0800

    Screenshot and Stylesheet prelimary commit
    --Screenshot function added
    --Added stylesheet to ExtraBrowser

--- Browser/ExtraBrowser/ExtraBrowserWindow.xaml.cs
+++ Browser/ExtraBrowser/ExtraBrowserWindow.xaml.cs
@@ -1,16 +1,19 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using System.Windows;
-using System.Windows.Input;
-using CefSharp;
-using Grpc.Core;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Web.WebView2.Core;
+using Windows.Graphics.Imaging;
 
 namespace Browser.ExtraBrowser;
 
 public partial class ExtraBrowserWindow : Window
 {
 	public FormBrowserTranslationViewModel FormBrowser { get; }
+	private CoreWebView2Frame? gameframe { get; set; }
+	private CoreWebView2Frame? kancolleframe { get; set; }
+	private string StyleClassID { get; } = Guid.NewGuid().ToString().Substring(0, 8);
 	public ExtraBrowserWindow()
 	{
 		FormBrowser = App.Current.Services.GetService<FormBrowserTranslationViewModel>()!;
@@ -26,9 +29,30 @@ public partial class ExtraBrowserWindow : Window
 		Browser.CoreWebView2.WebResourceRequested += CoreWebView2_WebResourceRequested;
 		Browser.CoreWebView2.WebMessageReceived += UpdateAddressBar;
 		Browser.CoreWebView2.AddWebResourceRequestedFilter("*", CoreWebView2WebResourceContext.Script);
-
+		Browser.CoreWebView2.NavigationCompleted += OnNavigationCompleted;
+		Browser.CoreWebView2.FrameCreated += OnFrameCreated;
 		SetCookie();
 		Browser.CoreWebView2.Navigate("http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/");
+	}
+
+	private void OnNavigationCompleted(object? sender, CoreWebView2NavigationCompletedEventArgs e)
+	{
+		if (e.IsSuccess)
+		{
+			SetStyleSheet();
+		}
+	}
+
+	private void OnFrameCreated(object? sender, CoreWebView2FrameCreatedEventArgs e)
+	{
+		if (e.Frame.Name.Contains(@"game_frame"))
+		{
+			gameframe = e.Frame;
+		}
+		if (e.Frame.Name.Contains(@"htmlWrap"))
+		{
+			kancolleframe = e.Frame;
+		}
 	}
 
 	private void CoreWebView2_WebResourceRequested(object? sender, CoreWebView2WebResourceRequestedEventArgs e)
@@ -36,6 +60,10 @@ public partial class ExtraBrowserWindow : Window
 		if (e.Request.Uri.Contains(@"gadget_html5"))
 		{
 			e.Request.Uri = e.Request.Uri.Replace("http://203.104.209.7/gadget_html5/", "https://kcwiki.github.io/cache/gadget_html5/");
+		}
+		if (e.Request.Uri.Contains("kcs2/resources/bgm"))
+		{
+			e.Request.Headers.RemoveHeader("Range");
 		}
 	}
 
@@ -87,6 +115,31 @@ public partial class ExtraBrowserWindow : Window
 		if (Browser.CoreWebView2.CanGoForward)
 		{
 			Browser.CoreWebView2.GoForward();
+		}
+	}
+	private void SetStyleSheet()
+	{
+		if (Browser is { IsInitialized: true })
+		{
+			if (gameframe != null)
+			{
+
+				Browser.ExecuteScriptAsync(String.Format(Properties.Resources.PageScript, StyleClassID));
+				gameframe.ExecuteScriptAsync(String.Format(Properties.Resources.FrameScript, StyleClassID));
+				gameframe.ExecuteScriptAsync("document.body.style.backgroundColor = \"#000000\";");
+				Browser.HorizontalAlignment = HorizontalAlignment.Center;
+				Browser.VerticalAlignment = VerticalAlignment.Center;
+				Browser.Width = 1200;
+				Browser.Height = 720;
+			}
+		}
+	}
+	private async void ScreenShotButtonClick(object sender, RoutedEventArgs e)
+	{
+		using (FileStream fileStream = File.Create("output.png"))
+		{
+			await Browser.CoreWebView2.CapturePreviewAsync(CoreWebView2CapturePreviewImageFormat.Png, fileStream).ConfigureAwait(false);
+			await fileStream.FlushAsync().ConfigureAwait(false);
 		}
 	}
 }
