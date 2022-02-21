366f22053f93678487b65ce2fc0c8b5bbf390d1e
myangelkamikaze <myangelkamikaze@gmail.com>
2022-02-22 04:01:40 +0900

    add accuracy test

---
+++ ElectronicObserverCoreTests/AccuracyTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using ElectronicObserver.Utility.Data;
+using ElectronicObserverTypes;
+using ElectronicObserverTypes.Mocks;
+using Xunit;
+
+namespace ElectronicObserverCoreTests;
+
+[Collection(DatabaseCollection.Name)]
+public class AccuracyTests
+{
+	private DatabaseFixture Db { get; }
+
+	public AccuracyTests(DatabaseFixture db)
+	{
+		Db = db;
+	}
+
+	[Fact]
+	public void AccuracyTest1()
+	{
+		IShipData bismarck = new ShipDataMock(Db.MasterShips[ShipId.BismarckDrei])
+		{
+			Level = 175,
+			Condition = 85,
+			LuckBase = 84,
+			AllSlotInstance = new List<IEquipmentData>
+			{
+				new EquipmentDataMock(Db.MasterEquipment[EquipmentId.MainGunLarge_46cmTripleGunKai])
+				{
+					Level = 7
+				},
+			},
+		};
+
+		IFleetData fleet = new FleetDataMock
+		{
+			FleetType = FleetType.Single,
+			MembersInstance = new(new List<IShipData>
+			{
+				bismarck
+			}),
+		};
+
+		double expected = 160;
+
+		Assert.Equal(expected, bismarck.GetDayAttackAccuracy(DayAttackKind.NormalAttack, fleet));
+	}
+}
