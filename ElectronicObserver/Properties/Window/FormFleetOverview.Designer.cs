8e8027ab9d51352cdcf85c51b869cb92b4eced51
gre4bee <gre4bee@gmail.com>
2022-04-07 14:11:33 +0800

    Fix JP tooltips
    --remove extraneous brackets in tooltips

--- ElectronicObserver/Properties/Window/FormFleetOverview.Designer.cs
+++ ElectronicObserver/Properties/Window/FormFleetOverview.Designer.cs
@@ -73,7 +73,7 @@ namespace ElectronicObserver.Properties.Window {
         /// <summary>
         ///   Looks up a localized string similar to ドラム缶搭載: {0}個
         ///大発動艇搭載: {1}個
-        ///水上電探: {10} ({11}}艦)
+        ///水上電探: {10} ({11}艦)
         ///輸送量(TP): S {2} / A {3}
         ///
         ///制空戦力合計: {4}
