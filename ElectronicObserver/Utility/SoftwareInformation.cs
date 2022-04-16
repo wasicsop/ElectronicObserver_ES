ac452d72ac349dabb6757d23142de5842570eae7
myangelkamikaze <myangelkamikaze@gmail.com>
2022-04-16 13:17:17 +0900

    automate UpdateTime

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using DynaJson;
@@ -42,7 +44,7 @@ public static class SoftwareInformation
 	/// <summary>
 	/// 更新日時
 	/// </summary>
-	public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2022/03/27 13:00:00");
+	public static DateTime UpdateTime => typeof(App).Assembly.GetLinkerTime();
 
 
 	private static System.Net.WebClient? Client { get; set; }
@@ -125,5 +127,26 @@ public static class SoftwareInformation
 		{
 			ErrorReporter.SendErrorReport(ex, Resources.UpdateConnectionFailed);
 		}
+	}
+
+	/// <summary>
+	/// Build time in JST
+	/// </summary>
+	/// <param name="assembly"></param>
+	/// <returns></returns>
+	private static DateTime GetLinkerTime(this Assembly assembly)
+	{
+		const string buildVersionMetadataPrefix = "+build";
+
+		AssemblyInformationalVersionAttribute? attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
+		string? value = attribute?.InformationalVersion;
+		int? index = value?.IndexOf(buildVersionMetadataPrefix);
+
+		if (index is not > 0) return default;
+		if (value is null) return default;
+
+		value = value[(index.Value + buildVersionMetadataPrefix.Length)..];
+		return DateTime.ParseExact(value, "yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture) + TimeSpan.FromHours(9);
+
 	}
 }
