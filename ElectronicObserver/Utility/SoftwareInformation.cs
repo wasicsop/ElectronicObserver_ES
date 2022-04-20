04e5fb5f06d30083bcf77de481428f1c1d78da19
myangelkamikaze <myangelkamikaze@gmail.com>
2022-04-20 20:34:57 +0900

    remove old build time method

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Globalization;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using DynaJson;
@@ -126,26 +124,5 @@ public static class SoftwareInformation
 		{
 			ErrorReporter.SendErrorReport(ex, Resources.UpdateConnectionFailed);
 		}
-	}
-
-	/// <summary>
-	/// Build time in JST
-	/// </summary>
-	/// <param name="assembly"></param>
-	/// <returns></returns>
-	private static DateTime GetLinkerTime(this Assembly assembly)
-	{
-		const string buildVersionMetadataPrefix = "+build";
-
-		AssemblyInformationalVersionAttribute? attribute = assembly.GetCustomAttribute<AssemblyInformationalVersionAttribute>();
-		string? value = attribute?.InformationalVersion;
-		int? index = value?.IndexOf(buildVersionMetadataPrefix);
-
-		if (index is not > 0) return default;
-		if (value is null) return default;
-
-		value = value[(index.Value + buildVersionMetadataPrefix.Length)..];
-		return DateTime.ParseExact(value, "yyyy-MM-ddTHH:mm:ss", CultureInfo.InvariantCulture) + TimeSpan.FromHours(9);
-
 	}
 }
