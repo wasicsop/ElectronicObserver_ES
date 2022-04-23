11ec9daf4574d9c784e8f7e4408bb7473dd28ce5
myangelkamikaze <myangelkamikaze@gmail.com>
2022-04-23 22:33:27 +0900

    api_req_combined_battle/goback_port property

--- ElectronicObserver/Observer/APIObserver.cs
+++ ElectronicObserver/Observer/APIObserver.cs
@@ -611,6 +611,13 @@ public sealed class APIObserver
 	public kcsapi.api_req_kousyou.remodel_slotlist ApiReqKousyou_RemodelSlotList =>
 		(kcsapi.api_req_kousyou.remodel_slotlist)this["api_req_kousyou/remodel_slotlist"];
 
+	/// <summary>
+	/// FCF <br />
+	/// <seealso href="https://github.com/andanteyk/ElectronicObserver/blob/develop/ElectronicObserver/Other/Information/apilist.txt#L3368" />
+	/// </summary>
+	public kcsapi.api_req_combined_battle.goback_port ApiReqCombinedBattle_GoBackPort =>
+		(kcsapi.api_req_combined_battle.goback_port)this["api_req_combined_battle/goback_port"];
+
 	#endregion
 
 	public string? ServerAddress { get; private set; }
