6708e13f0b58e01222e467296a6d8abc89827568
myangelkamikaze <myangelkamikaze@gmail.com>
2022-06-02 20:44:28 +0900

    fix jot init

--- ElectronicObserver/App.xaml.cs
+++ ElectronicObserver/App.xaml.cs
@@ -202,7 +202,7 @@ public partial class App : Application
 			.Property(w => w.Width)
 			.Property(w => w.WindowState)
 			.PersistOn(nameof(System.Windows.Window.Closed))
-			.PersistOn(nameof(System.Windows.Window.Closed));
+			.StopTrackingOn(nameof(System.Windows.Window.Closed));
 
 		// EventLockPlannerWindow extends System.Windows.Window so the config above applies to it
 		tracker
