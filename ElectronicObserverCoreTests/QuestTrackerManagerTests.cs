8e6f81fb43aa9fb154925fe4fddb8f0e8655b517
myangelkamikaze <myangelkamikaze@gmail.com>
2022-06-10 18:20:24 +0900

    add quest tracker manager tests

---
+++ ElectronicObserverCoreTests/QuestTrackerManagerTests.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using ElectronicObserver.Window.Dialog.QuestTrackerManager.Enums;
+using ElectronicObserver.Window.Dialog.QuestTrackerManager.Models.Conditions;
+using ElectronicObserver.Window.Dialog.QuestTrackerManager.ViewModels.Conditions;
+using ElectronicObserverTypes;
+using ElectronicObserverTypes.Mocks;
+using Xunit;
+
+namespace ElectronicObserverCoreTests;
+
+[Collection(DatabaseCollection.Name)]
+public class QuestTrackerManagerTests
+{
+	private DatabaseFixture Db { get; }
+
+	public QuestTrackerManagerTests(DatabaseFixture db)
+	{
+		Db = db;
+	}
+
+	[Fact]
+	public void ThisRemodelOrHigher()
+	{
+		ShipConditionModel yamatoCondition = new()
+		{
+			Id = ShipId.YamatoKaiNi,
+			MustBeFlagship = true,
+			RemodelComparisonType = RemodelComparisonType.AtLeast,
+		};
+
+		ShipTypeConditionModel clCondition = new()
+		{
+			Count = 1,
+			ComparisonType = ComparisonType.GreaterOrEqual,
+			Types = new ObservableCollection<ShipTypes>(new()
+			{
+				ShipTypes.LightCruiser,
+			})
+		};
+
+		ShipTypeConditionModel ddCondition = new()
+		{
+			Count = 2,
+			ComparisonType = ComparisonType.GreaterOrEqual,
+			Types = new ObservableCollection<ShipTypes>(new()
+			{
+				ShipTypes.Destroyer,
+			})
+		};
+
+		GroupConditionViewModel group = new(new()
+		{
+			GroupOperator = Operator.And,
+			Conditions = new ObservableCollection<ICondition?>(new()
+			{
+				yamatoCondition,
+				clCondition,
+				ddCondition,
+			})
+		});
+
+		FleetDataMock fleet = new()
+		{
+			MembersInstance = new ReadOnlyCollection<IShipData>(new List<IShipData>
+			{
+				new ShipDataMock(Db.MasterShips[ShipId.YamatoKaiNiJuu]),
+				new ShipDataMock(Db.MasterShips[ShipId.GambierBayMkII]),
+				new ShipDataMock(Db.MasterShips[ShipId.DeRuyterKai]),
+				new ShipDataMock(Db.MasterShips[ShipId.MurakumoKaiNi]),
+				new ShipDataMock(Db.MasterShips[ShipId.AkebonoKaiNi]),
+				new ShipDataMock(Db.MasterShips[ShipId.UshioKaiNi]),
+			})
+		};
+
+		Assert.True(group.ConditionMet(fleet));
+	}
+}
--- ElectronicObserverCoreTests/Startup.cs
+++ ElectronicObserverCoreTests/Startup.cs
@@ -1,12 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Windows.Documents;
 using CommunityToolkit.Mvvm.DependencyInjection;
 using ElectronicObserver.Database;
 using ElectronicObserver.TestData;
 using ElectronicObserver.Window.Tools.AutoRefresh;
 using ElectronicObserverTypes;
+using ElectronicObserverTypes.Data;
 using ElectronicObserverTypes.Mocks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -17,7 +17,33 @@ public class Startup
 {
 	public async void ConfigureServices(IServiceCollection services)
 	{
+		await using TestDataContext testDb = new();
+		Dictionary<ShipId, IShipDataMaster> masterShips = testDb.MasterShips
+			.Select(s => s.ToMasterShip())
+			.ToDictionary(s => s.ShipId);
+
+		foreach (ShipDataMasterMock ship in masterShips.Values.Cast<ShipDataMasterMock>())
+		{
+			if (ship.RemodelBeforeShipID > 0)
+			{
+				ship.RemodelBeforeShip = masterShips[(ShipId)ship.RemodelBeforeShipID];
+			}
+
+			if (ship.RemodelAfterShipID > 0)
+			{
+				ship.RemodelAfterShip = masterShips[(ShipId)ship.RemodelAfterShipID];
+			}
+		}
+
+		KCDatabaseMock kcdb = new();
+
+		foreach (IShipDataMaster ship in masterShips.Values)
+		{
+			kcdb.MasterShips.Add(ship);
+		}
+
 		Ioc.Default.ConfigureServices(new ServiceCollection()
+			.AddSingleton<IKCDatabase>(kcdb)
 			.AddSingleton<AutoRefreshTranslationViewModel>()
 			.BuildServiceProvider());
 
