01aaf628348a36896e4824ae2a35257fd9b79046
myangelkamikaze <myangelkamikaze@gmail.com>
2022-07-16 21:42:43 +0900

    add ship class display converter

---
+++ ElectronicObserver/Converters/ShipClassDisplayConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using ElectronicObserver.Data;
+using ElectronicObserverTypes;
+
+namespace ElectronicObserver.Converters;
+
+public class ShipClassDisplayConverter : IValueConverter
+{
+	public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		=> value switch
+		{
+			ShipClass.Unknown => "*",
+			ShipClass id => Constants.GetShipClass(id),
+			_ => throw new ArgumentOutOfRangeException()
+		};
+
+	public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+	{
+		throw new NotImplementedException();
+	}
+}
