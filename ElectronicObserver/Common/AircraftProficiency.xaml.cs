1cad2d90b5f99fd9650879b7aff86164986280da
myangelkamikaze <myangelkamikaze@gmail.com>
2022-07-31 00:55:40 +0900

    extract aircraft proficiency control

---
+++ ElectronicObserver/Common/AircraftProficiency.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace ElectronicObserver.Common;
+/// <summary>
+/// Interaction logic for AircraftProficiency.xaml
+/// </summary>
+public partial class AircraftProficiency : UserControl
+{
+	public static readonly DependencyProperty AircraftLevelProperty = DependencyProperty.Register(
+		nameof(AircraftLevel), typeof(int), typeof(AircraftProficiency), new PropertyMetadata(default(int)));
+
+	public int AircraftLevel
+	{
+		get => (int)GetValue(AircraftLevelProperty);
+		set => SetValue(AircraftLevelProperty, value);
+	}
+
+	public AircraftProficiency()
+	{
+		InitializeComponent();
+	}
+}
