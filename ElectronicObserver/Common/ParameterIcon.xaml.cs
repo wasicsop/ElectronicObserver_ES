ec2958a9d57e83f6e97c556a83c495603fef0fce
myangelkamikaze <myangelkamikaze@gmail.com>
2022-07-31 00:57:47 +0900

    add parameter icon control

---
+++ ElectronicObserver/Common/ParameterIcon.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ElectronicObserverTypes;
+
+namespace ElectronicObserver.Common;
+/// <summary>
+/// Interaction logic for ParameterIcon.xaml
+/// </summary>
+public partial class ParameterIcon : UserControl
+{
+	public static readonly DependencyProperty ParameterProperty = DependencyProperty.Register(
+		nameof(ParameterType), typeof(ParameterType), typeof(ParameterIcon), new PropertyMetadata(default(ParameterType)));
+
+	public ParameterType ParameterType
+	{
+		get => (ParameterType)GetValue(ParameterProperty);
+		set => SetValue(ParameterProperty, value);
+	}
+
+	public ParameterIcon()
+	{
+		InitializeComponent();
+	}
+}
