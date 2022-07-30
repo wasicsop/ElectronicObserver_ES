dd675a22f2b0aadef6ab4271f6ccff61457e1d80
myangelkamikaze <myangelkamikaze@gmail.com>
2022-07-31 00:59:01 +0900

    add parameter type enum

---
+++ ElectronicObserverTypes/ParameterType.cs
@@ -0,0 +1,23 @@
+﻿namespace ElectronicObserverTypes;
+
+public enum ParameterType
+{
+	Hp,
+	Firepower,
+	Torpedo,
+	AntiAir,
+	Armor,
+	AntiSubmarine,
+	Evasion,
+	Los,
+	Luck,
+	Bomber,
+	Accuracy,
+	Aircraft,
+	Speed,
+	Range,
+	Interception,
+	AntiBomber,
+	AircraftCost,
+	AircraftDistance,
+}
