97e70ad57e4f84d4ab60038cc6fda6fa38a19ca8
myangelkamikaze <myangelkamikaze@gmail.com>
2022-08-20 22:07:01 +0900

    add air base mocks

---
+++ ElectronicObserverTypes/Mocks/BaseAirCorpsDataMock.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace ElectronicObserverTypes.Mocks;
+
+public class BaseAirCorpsDataMock : IBaseAirCorpsData
+{
+	public int MapAreaID { get; set; }
+	public int AirCorpsID { get; set; }
+	public string Name { get; set; }
+	public int Distance { get; set; }
+	public int Bonus_Distance { get; set; }
+	public int Base_Distance { get; set; }
+	public AirBaseActionKind ActionKind { get; set; }
+	public List<int> StrikePoints { get; set; }
+	public IDictionary<int, IBaseAirCorpsSquadron> Squadrons { get; set; }
+	public int ID { get; set; }
+	public bool IsAvailable { get; set; }
+}
---
+++ ElectronicObserverTypes/Mocks/BaseAirCorpsSquadronMock.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ElectronicObserverTypes.Mocks;
+
+public class BaseAirCorpsSquadronMock : IBaseAirCorpsSquadron
+{
+	public int SquadronID { get; set; }
+	public int State { get; set; }
+	public int EquipmentMasterID { get; set; }
+	public IEquipmentData? EquipmentInstance { get; set; }
+	public int EquipmentID { get; set; }
+	public IEquipmentDataMaster? EquipmentInstanceMaster { get; set; }
+	public int AircraftCurrent { get; set; }
+	public int AircraftMax { get; set; }
+	public int Condition { get; set; }
+	public DateTime RelocatedTime { get; set; }
+	public int ID { get; set; }
+	public bool IsAvailable { get; set; }
+	public void LoadFromResponse(string apiname, object elem)
+	{
+		throw new NotImplementedException();
+	}
+}
