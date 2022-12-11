a670cb558d55b1b432ce90ef12805738d2324dad
myangelkamikaze <myangelkamikaze@gmail.com>
2022-12-11 12:43:00 +0900

    fix text color

--- ElectronicObserver/Window/FormBrowserHost.cs
+++ ElectronicObserver/Window/FormBrowserHost.cs
@@ -554,7 +554,7 @@ public partial class FormBrowserHost : Form
 			var image = ResourceManager.Instance.Icons.Images[(int)IconContent.ConditionVeryTired];
 			e.Graphics.DrawImage(image, new Rectangle(16, 16, 16, 16));
 
-			e.Graphics.DrawString(GeneralRes.BrowserProcessClickStart, Utility.Configuration.Config.UI.MainFont, Brushes.Black, new PointF(48, 16));
+			e.Graphics.DrawString(GeneralRes.BrowserProcessClickStart, Utility.Configuration.Config.UI.MainFont, new SolidBrush(Utility.Configuration.Config.UI.ForeColor), new PointF(48, 16));
 		}
 	}
 
