29a60a3f6ebcef57495f3f1edeb5b74224558db0
myangelkamikaze <myangelkamikaze@gmail.com>
2022-12-24 17:34:30 +0900

    text fix

--- ElectronicObserver/Notifier/NotifierRes.Designer.cs
+++ ElectronicObserver/Notifier/NotifierRes.Designer.cs
@@ -19,7 +19,7 @@ namespace ElectronicObserver.Notifier {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class NotifierRes {
@@ -277,7 +277,7 @@ namespace ElectronicObserver.Notifier {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to {0}は{1}に改造されます。.
+        ///   Looks up a localized string similar to {0}は{1}に改造出来ます。.
         /// </summary>
         public static string RemodelText {
             get {
