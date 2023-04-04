283abe9de82c8e622b01a9c6057bf71766c8fdca
MyAngelKamikaze <myangelkamikaze@gmail.com>
2023-04-04 20:00:49 +0900

    log luck gain on marriage

--- ElectronicObserver/LoggerRes.Designer.cs
+++ ElectronicObserver/LoggerRes.Designer.cs
@@ -19,7 +19,7 @@ namespace ElectronicObserver {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class LoggerRes {
@@ -459,7 +459,7 @@ namespace ElectronicObserver {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to {0} とケッコンカッコカリしました。おめでとうございます！.
+        ///   Looks up a localized string similar to {0} とケッコンカッコカリしました。おめでとうございます！(運+{1}→{2}/{3}).
         /// </summary>
         public static string JustMarried {
             get {
--- ElectronicObserver/Observer/kcsapi/api_req_kaisou/marriage.cs
+++ ElectronicObserver/Observer/kcsapi/api_req_kaisou/marriage.cs
@@ -4,24 +4,38 @@ namespace ElectronicObserver.Observer.kcsapi.api_req_kaisou;
 
 public class marriage : APIBase
 {
-
 	public override void OnResponseReceived(dynamic data)
 	{
-
-		Utility.Logger.Add(2, string.Format(LoggerRes.JustMarried, KCDatabase.Instance.Ships[(int)data.api_id].Name));
-
-		var db = KCDatabase.Instance;
+		KCDatabase db = KCDatabase.Instance;
 		int id = (int)data.api_id;
-		var ship = db.Ships[id];
+		ShipData? ship = db.Ships[id];
+
+		// should never be null but...
+		int luckBeforeMarriage = ship?.LuckTotal ?? 0;
 
 		if (ship != null)
+		{
 			ship.LoadFromResponse(APIName, data);
+		}
 		else
 		{
-			var a = new ShipData();
+			// todo: how would this ever happen?
+			ShipData a = new();
 			a.LoadFromResponse(APIName, data);
 			db.Ships.Add(a);
+			ship = db.Ships[id];
 		}
+
+		int luckAfterMarriage = ship.LuckTotal;
+
+		Utility.Logger.Add(2, string.Format
+		(
+			LoggerRes.JustMarried,
+			ship.Name,
+			luckAfterMarriage - luckBeforeMarriage,
+			ship.LuckBase, 
+			ship.MasterShip.LuckMax
+		));
 
 		base.OnResponseReceived((object)data);
 	}
