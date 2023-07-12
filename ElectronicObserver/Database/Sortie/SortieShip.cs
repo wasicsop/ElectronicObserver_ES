13b21bb20308ba0d158949156ce5dfcef43c7803
MyAngelKamikaze <myangelkamikaze@gmail.com>
2023-07-12 15:18:30 +0900

    unused using

--- ElectronicObserver/Database/Sortie/SortieShip.cs
+++ ElectronicObserver/Database/Sortie/SortieShip.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Text.Json.Serialization;
-using ElectronicObserver.KancolleApi.Types.Models;
 using ElectronicObserverTypes;
 
 namespace ElectronicObserver.Database.Sortie;
