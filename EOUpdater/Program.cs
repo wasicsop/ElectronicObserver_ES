a8ea3984fcc25bef5b7ec19d9e827378fda0d568
MyAngelKamikaze <myangelkamikaze@gmail.com>
2024-08-12 23:47:40 +0900

    rewrite EO updater

--- EOUpdater/Program.cs
+++ EOUpdater/Program.cs
@@ -1,293 +1,270 @@
-﻿using System;
-using System.CodeDom.Compiler;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Globalization;
-using System.IO;
 using System.IO.Compression;
-using System.Linq;
-using System.Net;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Json;
+using System.Net.Http.Json;
 using System.Security.Cryptography;
-using System.Text;
+using System.Text.Json.Serialization;
 
-namespace EOUpdater
+string appDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ElectronicObserver");
+const string updateUrl = "https://raw.githubusercontent.com/ElectronicObserverEN/Data/master/update.json";
+
+Language language = CultureInfo.CurrentCulture.Name switch
 {
-	enum Language
+	"ja-JP" => Language.Japanese,
+	_ => Language.English
+};
+
+Console.Title = language switch
+{
+	Language.Japanese => "七四式アップデータ",
+	_ => "EO updater"
+};
+
+bool wait = true;
+bool restart = false;
+
+foreach (string argument in args)
+{
+	switch (argument)
 	{
-		Japanese,
-		English
+		case "--nowait":
+			wait = false;
+			break;
+		case "--restart":
+			restart = true;
+			break;
+		default:
+			Console.WriteLine(argument + "is not a valid argument.");
+			break;
+	}
+}
+
+Directory.CreateDirectory(appDataFolder);
+
+try
+{
+	string tempFile = appDataFolder + @"\latest.zip";
+	string destPath = Path.GetDirectoryName(AppContext.BaseDirectory)!;
+
+	(string downloadUrl, string downloadHash) = await CheckUpdate(updateUrl);
+	await DownloadUpdate(downloadUrl, tempFile, downloadHash);
+
+	if (wait)
+	{
+		foreach (Process process in Process.GetProcessesByName("ElectronicObserver"))
+		{
+			Console.WriteLine(language switch
+			{
+				Language.Japanese => "七四式を閉じると、アップデート処理が開始されます。",
+				_ => "Close Electronic Observer to start the updating process."
+			});
+			process.WaitForExit();
+		}
+		foreach (Process process in Process.GetProcessesByName("EOBrowser"))
+		{
+			Console.WriteLine(language switch
+			{
+				Language.Japanese => "EOBrowserを閉じて、アップデート作業を開始します。",
+				_ => "Close EOBrowser to start the updating process."
+			});
+			process.WaitForExit();
+		}
 	}
 
-	internal class Program
+	DeleteOldFiles();
+	ExtractUpdate(tempFile, destPath);
+}
+catch (Exception e)
+{
+	Console.WriteLine(e);
+}
+
+if (restart)
+{
+	string appPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase!, "ElectronicObserver.exe");
+	Process.Start(appPath);
+	return;
+}
+
+string st = language switch
+{
+	Language.Japanese => "アップデートが完了しました。このウィンドウを閉じることができます。",
+	_ => "Update complete. You can close this window."
+};
+
+Console.WriteLine(st);
+Console.ReadKey();
+
+return;
+
+static void DeleteOldFiles()
+{
+	string eoFolderPath = Path.GetDirectoryName(AppContext.BaseDirectory)!;
+
+	IEnumerable<string> dllFiles = Directory.EnumerateFiles(eoFolderPath, "*.dll", SearchOption.TopDirectoryOnly);
+	IEnumerable<string> configFiles = Directory.EnumerateFiles(eoFolderPath, "*.runtimeconfig.json", SearchOption.TopDirectoryOnly);
+
+	foreach (string fileName in dllFiles.Concat(configFiles))
 	{
-		private static readonly string AppDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), @"ElectronicObserver");
-		private const string UpdateUrl = @"https://raw.githubusercontent.com/ElectronicObserverEN/Data/master/update.json";
-
-		private static Language Language { get; set; }
-
-		private static void Main(string[] args)
+		try
 		{
-			Language = CultureInfo.CurrentCulture.Name switch
-			{
-				"ja-JP" => Language.Japanese,
-				_ => Language.English
-			};
-
-			Console.Title = Language switch
-			{
-				Language.Japanese => "七四式アップデータ",
-				_ => "EO updater"
-			};
-
-			var wait = true;
-			var restart = false;
-
-			foreach (var argument in args)
-			{
-				switch (argument)
-				{
-					case "--nowait":
-						wait = false;
-						break;
-					case "--restart":
-						restart = true;
-						break;
-					default:
-						Console.WriteLine(argument + "is not a valid argument.");
-						break;
-				}
-			}
-
-			if (!Directory.Exists(AppDataFolder))
-				Directory.CreateDirectory(AppDataFolder);
-
-			try
-			{
-				var tempFile = AppDataFolder + @"\latest.zip";
-				var destPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase);
-
-				CheckUpdate(UpdateUrl, out var downloadUrl, out var downloadHash);
-				DownloadUpdate(downloadUrl, tempFile, downloadHash);
-
-				if (wait)
-				{
-					foreach (var process in Process.GetProcessesByName("ElectronicObserver"))
-					{
-						string s = Language switch
-						{
-							Language.Japanese => "七四式を閉じると、アップデート処理が開始されます。",
-							_ => "Close Electronic Observer to start the updating process."
-						};
-						Console.WriteLine(s);
-						process.WaitForExit();
-					}
-					foreach (var process in Process.GetProcessesByName("EOBrowser"))
-					{
-						string s = Language switch
-						{
-							Language.Japanese => "EOBrowserを閉じて、アップデート作業を開始します。",
-							_ => "Close EOBrowser to start the updating process."
-						};
-						Console.WriteLine(s);
-						process.WaitForExit();
-					}
-				}
-
-				DeleteOldFiles();
-				ExtractUpdate(tempFile, destPath);
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e);
-			}
-
-			if (restart)
-			{
-				var appPath = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, @"ElectronicObserver.exe");
-				Process.Start(appPath);
-				return;
-			}
-
-			string st = Language switch
-			{
-				Language.Japanese => "アップデートが完了しました。このウィンドウを閉じることができます。",
-				_ => "Update complete. You can close this window."
-			};
-			Console.WriteLine(st);
-			Console.ReadKey();
+			File.Delete(Path.Combine(eoFolderPath, fileName));
+			// Console.WriteLine($"Deleted {fileName}");
 		}
-
-		private static void DeleteOldFiles()
+		catch
 		{
-			string eoFolderPath = Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
-
-			IEnumerable<string> dllFiles = Directory.EnumerateFiles(eoFolderPath, "*.dll", SearchOption.TopDirectoryOnly);
-			IEnumerable<string> configFiles = Directory.EnumerateFiles(eoFolderPath, "*.runtimeconfig.json", SearchOption.TopDirectoryOnly);
-
-			foreach (string fileName in dllFiles.Concat(configFiles))
-			{
-				try
-				{
-					File.Delete(Path.Combine(eoFolderPath, fileName));
-					// Console.WriteLine($"Deleted {fileName}");
-				}
-				catch
-				{
-					// Console.WriteLine($"Failed to delete {fileName}");
-				}
-			}
-		}
-
-		private static void ExtractUpdate(string zipPath, string extractPath)
-		{
-			var localPath = new Uri(extractPath).LocalPath;
-			using (var archive = ZipFile.Open(zipPath, ZipArchiveMode.Read))
-			{
-				string s = Language switch
-				{
-					Language.Japanese => "抽出開始...",
-					_ => "Start extracting..."
-				};
-				Console.WriteLine(s);
-				foreach (var file in archive.Entries)
-				{
-					var fullname = file.FullName.Replace(@"ElectronicObserver/", "");
-					var completeFileName = Path.Combine(localPath, fullname);
-					var directory = Path.GetDirectoryName(completeFileName);
-
-					if (directory != null && !Directory.Exists(directory))
-						Directory.CreateDirectory(directory);
-
-					if (file.Name == "DynamicJson.dll" || file.Name == "EOUpdater.exe" || file.Name == "") continue;
-
-					try
-					{
-						file.ExtractToFile(completeFileName, true);
-					}
-					catch (Exception)
-					{
-						Console.WriteLine($"Couldn't update {fullname}");
-					}
-				}
-				s = Language switch
-				{
-					Language.Japanese => "抽出終了。",
-					_ => "Extracting finished."
-				};
-				Console.WriteLine(s);
-			}
-		}
-
-		private static string GetHash(string filename)
-		{
-			using (var sha256 = SHA256.Create())
-			{
-				using (var stream = File.OpenRead(filename))
-				{
-					var hash = sha256.ComputeHash(stream);
-					return BitConverter.ToString(hash).Replace("-", "");
-				}
-			}
-		}
-
-		private static void CheckUpdate(string url, out string downloadUrl, out string downloadHash)
-		{
-			using (var client = new WebClient())
-			{
-				ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-				string str = client.DownloadString(url);
-				MemoryStream updateData = new(Encoding.UTF8.GetBytes(str));
-
-				DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Rootobject));
-				Rootobject json = (Rootobject)serializer.ReadObject(updateData);
-
-				string s = Language switch
-				{
-					Language.Japanese => "== 更新のデータ ==",
-					_ => "== Update Data =="
-				};
-				Console.WriteLine(s);
-				Console.WriteLine(json.bld_date);
-				Console.WriteLine(json.ver);
-				Console.WriteLine(json.note.Replace("<br>", "\r\n"));
-				Console.WriteLine(json.url);
-				s = Language switch
-				{
-					Language.Japanese => "ハッシュ",
-					_ => "Hash"
-				};
-				Console.WriteLine($"{s}: {json.hash}");
-				Console.WriteLine("==========\r\n");
-
-				downloadUrl = json.url;
-				downloadHash = json.hash;
-
-
-			}
-		}
-
-		private static void DownloadUpdate(string downloadUrl, string tempFile, string downloadHash)
-		{
-			if (!File.Exists(tempFile) || GetHash(tempFile) != downloadHash)
-				DownloadUpdate(downloadUrl, tempFile);
-
-			string s = Language switch
-			{
-				Language.Japanese => "ファイル",
-				_ => "File"
-			};
-			Console.WriteLine($"{s}: latest.zip SHA-256: {GetHash(tempFile)}");
-
-			s = Language switch
-			{
-				Language.Japanese => "ダウンロード完了。",
-				_ => "Download complete."
-			};
-			Console.WriteLine($"{s}\r\n");
-		}
-
-		private static void DownloadUpdate(string url, string tempFile)
-		{
-			try
-			{
-				using (var client = new WebClient())
-				{
-					ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-					string s = Language switch
-					{
-						Language.Japanese => "アップデートをダウンロード中...",
-						_ => "Downloading update..."
-					};
-					Console.WriteLine(s);
-					client.DownloadFile(url, tempFile);
-				}
-			}
-			catch (Exception e)
-			{
-				Console.WriteLine(e);
-			}
-
-		}
-
-		[DataContract]
-		public class Rootobject
-		{
-			[DataMember]
-			public string bld_date { get; set; }
-			[DataMember]
-			public string ver { get; set; }
-			[DataMember]
-			public string note { get; set; }
-			[DataMember]
-			public string url { get; set; }
-			[DataMember]
-			public string hash { get; set; }
-			[DataMember]
-			public string kancolle_mt { get; set; }
-			[DataMember]
-			public int event_state { get; set; }
+			// Console.WriteLine($"Failed to delete {fileName}");
 		}
 	}
+}
+
+void ExtractUpdate(string zipPath, string extractPath)
+{
+	string localPath = new Uri(extractPath).LocalPath;
+	using ZipArchive archive = ZipFile.Open(zipPath, ZipArchiveMode.Read);
+
+	Console.WriteLine(language switch
+	{
+		Language.Japanese => "抽出開始...",
+		_ => "Start extracting..."
+	});
+
+	foreach (ZipArchiveEntry file in archive.Entries)
+	{
+		string fullname = file.FullName.Replace("ElectronicObserver/", "");
+		string completeFileName = Path.Combine(localPath, fullname);
+		string? directory = Path.GetDirectoryName(completeFileName);
+
+		if (directory != null && !Directory.Exists(directory))
+		{
+			Directory.CreateDirectory(directory);
+		}
+
+		if (file.Name is "DynamicJson.dll" or "EOUpdater.exe" or "") continue;
+
+		try
+		{
+			file.ExtractToFile(completeFileName, true);
+		}
+		catch (Exception)
+		{
+			Console.WriteLine($"Couldn't update {fullname}");
+		}
+	}
+
+	Console.WriteLine(language switch
+	{
+		Language.Japanese => "抽出終了。",
+		_ => "Extracting finished."
+	});
+}
+
+static string GetHash(string filename)
+{
+	using SHA256 sha256 = SHA256.Create();
+	using FileStream stream = File.OpenRead(filename);
+
+	byte[] hash = sha256.ComputeHash(stream);
+	return BitConverter.ToString(hash).Replace("-", "");
+}
+
+async Task<(string downloadUrl, string downloadHash)> CheckUpdate(string url)
+{
+	using HttpClient client = new();
+
+	UpdateData json = await client.GetFromJsonAsync(url, SourceGenerationContext.Default.UpdateData)
+		?? throw new Exception("Failed to download update data.");
+
+	Console.WriteLine(language switch
+	{
+		Language.Japanese => "== 更新のデータ ==",
+		_ => "== Update Data =="
+	});
+	Console.WriteLine(json.BuildDate);
+	Console.WriteLine(json.Version);
+	Console.WriteLine(json.Note.Replace("<br>", "\r\n"));
+	Console.WriteLine(json.Url);
+	string hash = language switch
+	{
+		Language.Japanese => "ハッシュ",
+		_ => "Hash"
+	};
+	Console.WriteLine($"{hash}: {json.Hash}");
+	Console.WriteLine("==========\r\n");
+
+	return (json.Url, json.Hash);
+}
+
+async Task DownloadUpdate(string downloadUrl, string tempFile, string downloadHash)
+{
+	if (!File.Exists(tempFile) || GetHash(tempFile) != downloadHash)
+	{
+		await DownloadUpdate2(downloadUrl, tempFile);
+	}
+
+	string s = language switch
+	{
+		Language.Japanese => "ファイル",
+		_ => "File"
+	};
+	Console.WriteLine($"{s}: latest.zip SHA-256: {GetHash(tempFile)}");
+
+	s = language switch
+	{
+		Language.Japanese => "ダウンロード完了。",
+		_ => "Download complete."
+	};
+	Console.WriteLine($"{s}\r\n");
+}
+
+async Task DownloadUpdate2(string url, string tempFile)
+{
+	try
+	{
+		using HttpClient client = new();
+
+		Console.WriteLine(language switch
+		{
+			Language.Japanese => "アップデートをダウンロード中...",
+			_ => "Downloading update..."
+		});
+
+		await using Stream stream = await client.GetStreamAsync(url);
+		await using FileStream fs = new(tempFile, FileMode.CreateNew);
+
+		await stream.CopyToAsync(fs);
+	}
+	catch (Exception e)
+	{
+		Console.WriteLine(e);
+	}
+
+}
+
+[JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(UpdateData))]
+internal partial class SourceGenerationContext : JsonSerializerContext;
+
+public class UpdateData
+{
+	[JsonPropertyName("bld_date")]
+	public required string BuildDate { get; init; }
+
+	[JsonPropertyName("ver")]
+	public required string Version { get; init; }
+
+	[JsonPropertyName("note")]
+	public required string Note { get; init; }
+
+	[JsonPropertyName("url")]
+	public required string Url { get; init; }
+
+	[JsonPropertyName("hash")]
+	public required string Hash { get; init; }
+}
+
+enum Language
+{
+	Japanese,
+	English
 }
