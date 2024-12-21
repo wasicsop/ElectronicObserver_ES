eaa84a6ea190bc7ae491487b028eac382c4417fb
MyAngelKamikaze <myangelkamikaze@gmail.com>
2024-12-21 15:03:09 +0900

    add simplified chinese

--- ElectronicObserver/Window/Settings/UI/ConfigurationUITranslationViewModel.cs
+++ ElectronicObserver/Window/Settings/UI/ConfigurationUITranslationViewModel.cs
@@ -36,6 +36,7 @@ public class ConfigurationUITranslationViewModel : TranslationBaseViewModel
 	public string Language_English => ConfigurationResources.Language_English;
 	public string Language_Japanese => ConfigurationResources.Language_Japanese;
 	public string Language_Korean => ConfigurationResources.Language_Korean;
+	public string Language_SimplifiedChinese => ConfigurationResources.Language_SimplifiedChinese;
 
 	public string UI_RestartHint => ConfigurationResources.UI_RestartHint;
 }
