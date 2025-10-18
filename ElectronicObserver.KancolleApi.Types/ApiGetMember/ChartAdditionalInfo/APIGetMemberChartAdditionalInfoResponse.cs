50f01ab495892f62ce265174ffd13f6dd17ede7b
Jebzou <22751386+Jebzou@users.noreply.github.com>
2025-10-18 10:13:58 +0200

    api_get_member/chart_additional_info API reference

---
+++ ElectronicObserver.KancolleApi.Types/ApiGetMember/ChartAdditionalInfo/APIGetMemberChartAdditionalInfoResponse.cs
@@ -0,0 +1,9 @@
+﻿using ElectronicObserver.KancolleApi.Types.Models;
+
+namespace ElectronicObserver.KancolleApi.Types.ApiGetMember.ChartAdditionalInfo;
+
+public class APIGetMemberChartAdditionalInfoResponse
+{
+	[JsonPropertyName("api_deck_param")]
+	public List<ApiDeckParam> ApiDeckParam { get; set; } = [];
+}
